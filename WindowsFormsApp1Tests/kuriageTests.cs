6ffd2f067e67504eba124886178c82a7fb15c49e
広大 菅野 <190006@jc-21.jp>
2022-02-15 15:38:49 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1Tests/kuriageTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Tests
+{
+    [TestClass()]
+    public class kuriageTests
+    {
+        [TestMethod()]
+        public void kuriage10Test()
+        {
+            //Assert.Fail();
+            Assert.AreEqual(10, kuriage.kuriage10(6));
+            Assert.AreEqual(20, kuriage.kuriage10(20));
+            Assert.AreEqual(-20, kuriage.kuriage10(-18));
+        }
+    }
+}
